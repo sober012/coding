d7183bf0b843137bbcc0507b851a1add95d7e7ca
sober012 <sober012@naver.com>
2023-06-18 18:45:16 +0900

    2023/06/18

---
+++ 섹션1(자료형)/주석.py
@@ -0,0 +1,13 @@
+# 주석
+
+# ctrl + / 하면 한번에 주석
+# print("우리집 " + animal +"의 이름은 " + name +"예요")
+# print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요")
+# print(name +  "는 어른일까요? " +  str(is_adult))
+
+
+'''
+print("우리집 " + animal +"의 이름은 " + name +"예요")
+print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요")
+print(name +  "는 어른일까요? " +  str(is_adult))
+'''
