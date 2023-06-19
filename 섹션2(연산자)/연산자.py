3eea0e0319bed0d758ba9a5ca4e22d1fb2577dcd
sober012 <sober012@naver.com>
2023-06-19 12:31:18 +0900

    2023/06/19

---
+++ 섹션2(연산자)/연산자.py
@@ -0,0 +1,41 @@
+# 연산자
+
+print(1+1)
+print(3-1)
+print(5*2)
+print(3/3)
+print()
+
+print(2**3) # 2^3
+print(5%3) # 나머지
+print(5//3) # 몫
+print()
+
+print(10 > 3)
+print(4 >= 7)
+print(7 >= 7)
+print()
+
+print(3 == 3)
+print(4 == 2)
+print(3 + 4 == 7)
+print()
+
+print(2 != 3)
+print(not ( 3 != 2))
+print()
+
+print((3 > 0) and (3 < 5))
+print((3 > 0) & (3 < 5))
+print((3 > 0) or (3 < 5))
+print((3 > 0) | (3 < 5))
+print()
+
+print (3 > 2 < 1)
+print (5 > 2 < 6)
+
+
+
+
+
+
